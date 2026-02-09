7b10ba03a62b711b3ae77a20b6175d879aed9e15
Barış Harorlı <barik@Barss-MacBook-Pro.local>
2026-02-10 02:42:51 +0300

    feat(scaffold): initialize Tempo Passkey P2P Wallet project

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const PATHUSD_ADDRESS = '0x20c0000000000000000000000000000000000000' as const
+export const FEE_MANAGER_ADDRESS = '0xfeec000000000000000000000000000000000000' as const
+export const PATHUSD_DECIMALS = 6
+export const CHAIN_ID = 42431
+export const EXPLORER_URL = 'https://explore.tempo.xyz'
